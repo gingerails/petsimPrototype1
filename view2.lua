90525f3e3439d84e308222f944c7aaec3f6ccf63
gingerails <rocketcityabi@gmail.com>
2021-11-18 13:35:04 -0600

    Put panel into its own file.

--- view2.lua
+++ view2.lua
@@ -7,109 +7,14 @@
 local composer = require( "composer" )
 local widget = require( "widget" )
 local scene = composer.newScene()
+local panelstuff = require ( "panel" )
 
 ---------------------------------------------------------------------------------
---DROPDOWN PANEL WIDGET, 
--- This could be used for bath/inventory/shop
--- Could also make this its own file class, but I'm not doing that right now.
+--DROPDOWN PANEL WIDGET
+-- This section creates the panel widget, checks that it is shown (panelTransDone),
+-- and creates the panel open/close buttons. It references the panel file.
+-- Put inventory objects here unless we make the inventory OOP?
 ---------------------------------------------------------------------------------
-function widget.newPanel( options )
-    local customOptions = options or {}
-    local opt = {}
- 
-    opt.location = customOptions.location or "top"
- 
-    local default_width, default_height
-    if ( opt.location == "top" or opt.location == "bottom" ) then
-        default_width = display.contentWidth
-        default_height = display.contentHeight * 0.33
-    else
-        default_width = display.contentWidth * 0.33
-        default_height = display.contentHeight
-    end
- 
-    opt.width = customOptions.width or default_width
-    opt.height = customOptions.height or default_height
- 
-    opt.speed = customOptions.speed or 500
-    opt.inEasing = customOptions.inEasing or easing.linear
-    opt.outEasing = customOptions.outEasing or easing.linear
- 
-    if ( customOptions.onComplete and type(customOptions.onComplete) == "function" ) then
-        opt.listener = customOptions.onComplete
-    else 
-        opt.listener = nil
-    end
- 
-    local container = display.newContainer( opt.width, opt.height )
- 
-    if ( opt.location == "left" ) then
-        container.anchorX = 1.0
-        container.x = display.screenOriginX
-        container.anchorY = 0.5
-        container.y = display.contentCenterY
-    elseif ( opt.location == "right" ) then
-        container.anchorX = 0.0
-        container.x = display.actualContentWidth
-        container.anchorY = 0.5
-        container.y = display.contentCenterY
-    elseif ( opt.location == "top" ) then
-        container.anchorX = 0.5
-        container.x = display.contentCenterX
-        container.anchorY = 1.0
-        container.y = display.screenOriginY
-    else
-        container.anchorX = 0.5
-        container.x = display.contentCenterX
-        container.anchorY = 0.0
-        container.y = display.actualContentHeight
-    end
- 
-    function container:show()
-        local options = {
-            time = opt.speed,
-            transition = opt.inEasing
-        }
-        if ( opt.listener ) then
-            options.onComplete = opt.listener
-            self.completeState = "shown"
-        end
-        if ( opt.location == "top" ) then
-            options.y = display.screenOriginY + opt.height
-        elseif ( opt.location == "bottom" ) then
-            options.y = display.actualContentHeight - opt.height
-        elseif ( opt.location == "left" ) then
-            options.x = display.screenOriginX + opt.width
-        else
-            options.x = display.actualContentWidth - opt.width
-        end 
-        transition.to( self, options )
-    end
- 
-    function container:hide()
-        local options = {
-            time = opt.speed,
-            transition = opt.outEasing
-        }
-        if ( opt.listener ) then
-            options.onComplete = opt.listener
-            self.completeState = "hidden"
-        end
-        if ( opt.location == "top" ) then
-            options.y = display.screenOriginY
-        elseif ( opt.location == "bottom" ) then
-            options.y = display.actualContentHeight
-        elseif ( opt.location == "left" ) then
-            options.x = display.screenOriginX
-        else
-            options.x = display.actualContentWidth
-        end 
-        transition.to( self, options )
-    end
- 
-    return container
-end
-
 local function panelTransDone( target )
     --native.showAlert( "Panel", "Complete", { "Okay" } )
     if ( target.completeState ) then
@@ -144,7 +49,6 @@ local function handleButtonEvent( event )
 		panel:hide();
 	end
 end
---local startButton = display.newImageRect("start.png", 100, 100)
 
 panel.exitPanel = widget.newButton(
 	{
@@ -157,7 +61,7 @@ panel.exitPanel = widget.newButton(
 panel:insert (panel.exitPanel)
 ---------------------------------------------------------------------------------
 -- END PANEL CODE
--- Once again, probably should be in its own file.
+-- 
 ---------------------------------------------------------------------------------
 --
 --
